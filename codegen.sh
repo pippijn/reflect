7b2eef19001280594d70b8e76e1cfd10bd371a0d
Richard Molitor <gattschardo@gmail.com>
2010-07-31 11:49:48 +0200

    reorder codegen.sh

--- codegen.sh
+++ codegen.sh
@@ -19,10 +19,6 @@ do
   header=$base.h
   src=$base.c
 
-  echo "  AST_$ucase," >> src/ast/kinds.h
-  echo "#include \"$header\"" >> src/ast/nodes.h
-  echo "#include <ast/$header>" >> include/ast/nodes.h
-
   if [ -f ./include/ast/$header ]
   then
     continue		# remove this, if you want to diff
@@ -31,6 +27,11 @@ do
   fi
 
   echo "  GEN  $base"
+
+  echo "  AST_$ucase," >> src/ast/kinds.h
+  echo "#include \"$header\"" >> src/ast/nodes.h
+  echo "#include <ast/$header>" >> include/ast/nodes.h
+
   ./include/ast/_templ_header.pl $argv > ./include/ast/$header
   ./src/ast/_templ_header.pl $argv > ./src/ast/$header
   ./src/ast/_templ_source.pl $argv > ./src/ast/$src
