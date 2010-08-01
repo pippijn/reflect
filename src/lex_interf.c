052f0373f5ef0de5e36f9a9b6d7b7c3c84084e23
Pippijn van Steenhoven <pip88nl@gmail.com>
2010-08-01 13:51:55 +0200

    added some overflow checks

--- src/lex_interf.c
+++ src/lex_interf.c
@@ -3,6 +3,12 @@
 void
 lexer_update_lloc (YYLTYPE *yylloc, int lineno, int column, int leng)
 {
+  assert (yylloc != NULL);
+  assert (lineno >= 1);
+  assert (column >= 0);
+  assert (leng >= 1);
+  assert (UINT_MAX - column - leng > INT_MAX);
+
   yylloc->first_line = lineno;
   yylloc->first_column = column;
   yylloc->last_column = column + leng;
