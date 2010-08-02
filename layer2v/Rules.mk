dd4b84cd398450f392354c9a20fba1ba29bcabe2
Pippijn van Steenhoven <pip88nl@gmail.com>
2010-08-02 02:16:51 +0200

    use libxml2 for xml output

--- layer2v/Rules.mk
+++ layer2v/Rules.mk
@@ -1,10 +1,14 @@
-layer2v_CFLAGS	:= $(layer1_CFLAGS) -Ilayer2v/include
+layer2v_INCLUDE	:= $(layer1_CFLAGS) -Ilayer2v/include
+
+layer2v_CFLAGS	:= $(layer2v_INCLUDE) $(shell pkg-config --cflags libxml-2.0)
+layer2v_LDFLAGS	:= -llayer1 -llayer0 $(shell pkg-config --libs libxml-2.0)
+
 layer2v_SOURCES	:= $(shell find layer2v/src -name "*.c")
 layer2v_OBJECTS	:= $(addsuffix .o,$(basename $(layer2v_SOURCES)))
 
 bin/liblayer2v.so: $(layer2v_OBJECTS) bin/liblayer1.so bin/liblayer0.so
 	@echo "   CCLD   " $@
-	@$(LINK.c) -shared $(layer2v_OBJECTS) -llayer1 -llayer0 -o $@
+	@$(LINK.c) -shared $(layer2v_OBJECTS) $(layer2v_LDFLAGS) -o $@
 
 layer2v/%.o: layer2v/%.c
 	@echo "   CC     " $@
